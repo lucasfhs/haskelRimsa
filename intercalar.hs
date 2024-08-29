d4ca9493c4705702f661f067f43a047744e32d3c
lucashfs <lucashfsdev@gmail.com>
2024-08-29 09:44:58 -0300

    feat/add intercalar.hs

--- intercalar.hs
+++ intercalar.hs
@@ -2,3 +2,10 @@
 -- resultante da intercalação delas.
 -- Ex.: > intercalar [1,5,10] [2,7,9,20,25]
 -- [1,2,5,7,9,10,20,25]
+
+intercalar :: [Int] -> [Int] -> [Int]
+intercalar [] ys = ys
+intercalar xs [] = xs
+intercalar (x:xs) (y:ys)
+    | x < y     = x : intercalar xs (y:ys)
+    | otherwise = y : intercalar (x:xs) ys
